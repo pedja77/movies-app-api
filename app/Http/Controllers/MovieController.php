63d599b45b631a6fd46cfc2c8feb256f39df69b3
pedja77 <pedja77@stcable.net>
2018-05-22 11:39:59 +0200

    Zadatak 2

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Movie;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Movie::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $movie = new Movie();
+        $movie->title = $request->input('title');
+        $movie->director = $request->input('director');
+        $movie->imageUrl = $request->input('imageUrl');
+        $movie->duration = $request->input('duration');
+        $movie->releaseDate = $request->input('releaseDate');
+        $movie->genre = $request->input('genre');
+        
+        $movie->save();
+        return $movie;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Movie::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $movie = Movie::find($id);
+        $movie->title = $request->input('title');
+        $movie->director = $request->input('director');
+        $movie->imageUrl = $request->input('imageUrl');
+        $movie->duration = $request->input('duration');
+        $movie->releaseDate = $request->input('releaseDate');
+        $movie->genre = $request->input('genre');
+        
+        $movie->save();
+        return $movie;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $movie = Movie::find($id);
+        $movie->delete();
+        return new JsonResponse(true);
+    }
+}
