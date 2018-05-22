d8aac621fdf2d8a3d15b0968c3418b7fa6dddfe5
pedja77 <pedja77@stcable.net>
2018-05-22 13:14:23 +0200

    Zadatak 5

--- app/Movie.php
+++ app/Movie.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    public static function search($searchTerm) {
+        if (!$searchTerm) {
+            return self::all();
+        } else {
+            return self::where('title', 'LIKE', '%'.$searchTerm.'%')->get();
+        }
+        
+    }
 }
